934d777df5c2893d052beb59f397ad9984d2a7a2
charlie <esculin.hao@gmail.com>
2018-11-03 12:35:15 -0500

    modified package.json

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/frontend'));
+app.use(express.static(path.join(__dirname, 'dist', 'frontend')));
 
-// Start the app by listening on the default Heroku port
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'frontend', 'index.html'));
+});
+
 app.listen(process.env.PORT|| 8080);
