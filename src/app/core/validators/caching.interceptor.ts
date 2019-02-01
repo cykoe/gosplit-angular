0c1518357452340c7f9b15e866a51b2190b0b3d4
charlie <esculin.hao@gmail.com>
2019-02-01 15:11:41 -0600

    Caching interceptors are done. Will work on merge recipet module within app module

--- src/app/core/validators/caching.interceptor.ts
+++ src/app/core/validators/caching.interceptor.ts
@@ -1,13 +1,40 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { RequestCacheService } from '../services/request-cache.service';
 
 @Injectable()
 export class CachingInterceptor implements HttpInterceptor {
+  constructor(private cache: RequestCacheService) {
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (!isCachable(req)) console.log('ad');
-    return next.handle(req);
+    if (!isCachable(req)) {
+      return next.handle(req);
+    }
+    console.log(this.cache);
+    const cachedResponse = this.cache.get(req);
+    return cachedResponse ? of(cachedResponse) : sendRequest(req, next, this.cache);
   }
+}
+
+function isCachable(req: HttpRequest<any>) {
+  return req.method === 'GET';
+}
+
+function sendRequest(
+  req: HttpRequest<any>,
+  next: HttpHandler,
+  cache: RequestCacheService): Observable<HttpEvent<any>> {
+  const noHeadersReq = req.clone({setHeaders: {}});
+
+  return next.handle(noHeadersReq).pipe(
+    tap(event => {
+      if (event instanceof HttpResponse) {
+        cache.put(req, event);
+      }
+    })
+  );
 }
